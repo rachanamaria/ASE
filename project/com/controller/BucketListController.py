3df520a12fc14d8ea6dd02a5a921dd73df525dd2
rutvijgadhiya97 <rutvijgadhiya97@gmail.com>
2023-04-13 01:54:27 -0500

    Bucket list UI changes

--- project/com/controller/BucketListController.py
+++ project/com/controller/BucketListController.py
@@ -11,14 +11,16 @@ bucketlistdao=BucketListDAO()
 
 @app.route('/bucketlist', methods=['POST','GET'])
 def Bucketlist():
-    bucket_list = BucketListVo.query.all()
-    return render_template('BucketList.html',bucket_list=bucket_list) 
+    if 'isLoggedIn'in session and session['isLoggedIn']:
+        userId = session['userId']
+    bucket_list = BucketListVo.query.filter_by(UserId = userId).all()
+    completedcount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=True).count()
+    inprogresscount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=False).count()
+    
+    return render_template('BucketList.html',bucket_list=bucket_list,UserId=userId,Completedcount=completedcount,Inprogresscount=inprogresscount)
     
 
-@app.route('/loadBucketList', methods=['POST','GET'])
-def addlist():
-    UserId = request.form['UserId']
-    return redirect('/BucketList',UserId=UserId)
+
 
 @app.route('/addBucketList')
 def addBucketList():
@@ -38,27 +40,41 @@ def addList():
         objVo.isCompleted=False
         objVo.Purpose=request.form['Visibility']
         bucketlistdao.add(objVo)
-
-        bucket_list = BucketListVo.query.all()
-        return render_template('BucketList.html',bucket_list=bucket_list)  
-
+        print(UserId)
+        bucket_list = BucketListVo.query.filter_by(UserId = UserId).all()
+        completedcount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=True).count()
+        inprogresscount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=False).count()
+    
+        return render_template('BucketList.html',bucket_list=bucket_list,UserId=userId,Completedcount=completedcount,Inprogresscount=inprogresscount)
+    
 
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
+
+    if 'isLoggedIn'in session and session['isLoggedIn']:
+        userId = session['userId']
     bucket_list_item = BucketListVo.query.get_or_404(id)
     bucketlistdao.delete(bucket_list_item)
-    bucket_list = BucketListVo.query.all()
-    return render_template('BucketList.html',bucket_list=bucket_list)  
-
+    bucket_list = BucketListVo.query.filter_by(UserId = userId).all()
+    completedcount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=True).count()
+    inprogresscount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=False).count()
+    
+    return render_template('BucketList.html',bucket_list=bucket_list,UserId=userId,Completedcount=completedcount,Inprogresscount=inprogresscount)
+    
 @app.route('/completed/<int:id>',methods=['POST'])
 def completed(id):
     print("Completed")
+    if 'isLoggedIn'in session and session['isLoggedIn']:
+        userId = session['userId']
     bucket_list_item = BucketListVo.query.get_or_404(id)
     bucket_list_item.isCompleted = True
     today = date.today()
     # bucket_list.CompletionDate=today
     bucketlistdao.UpdateComplte(bucket_list_item)
-    bucket_list = BucketListVo.query.all()
+    bucket_list = BucketListVo.query.filter_by(UserId = userId).all()
+    completedcount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=True).count()
+    inprogresscount = BucketListVo.query.filter_by(UserId = userId).filter_by(isCompleted=False).count()
     
-    return render_template('BucketList.html',bucket_list=bucket_list)   
+    return render_template('BucketList.html',bucket_list=bucket_list,UserId=userId,Completedcount=completedcount,Inprogresscount=inprogresscount)
+       
